beacb520789d691c241488869075df26f9868b9a
NoPYXN <yunfournes@gmail.com>
2024-10-10 12:02:24 +0200

    modif header

--- header.js
+++ header.js
@@ -4,8 +4,8 @@ function createHeader() {
         <img src="../images/logo.png" alt="Logo Pet'Care" class="logo" onclick="window.location.href='index.php'">
         <nav class="nav-links">
             <a href="index.php">Accueil</a>
-            <a href="FormulaireInformations.php">Gérer les informations</a>
-            <a href="FormulaireVisit.php">Ajouter des informations médicales</a>
+            <a href="QrCode.html">Gérer les informations</a>
+            <a href="Achat.html">Ajouter des informations médicales</a>
             <a href="Contact.html">Contact</a>
         </nav>
         <div class="profile-icon" onclick="window.location.href='UserProfil.php'">
